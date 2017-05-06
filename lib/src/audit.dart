e0c9cb42a77ecc86a3c175c7cafbb78fb9d80244
Jonah Williams <campfish91@gmail.com>
2017-05-05 20:14:27 -0700

    woops

---
+++ lib/src/audit.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import 'dart:async';
+
+/// Creates a StreamTransformer which only emits once per [duration],
+/// at the end of the period.
+///
+StreamTransformer<T, T> audit<T>(Duration duration) => _auditer(duration);
+
+StreamTransformer<T, T> _auditer<T>(Duration duration) {
+  Timer timer;
+  bool shouldClose = false;
+  T recentData;
+
+  return new StreamTransformer.fromHandlers(
+      handleData: (T data, EventSink<T> sink) {
+    recentData = data;
+    if (timer == null) {
+      timer = new Timer(duration, () {
+        sink.add(recentData);
+        timer = null;
+        if (shouldClose) {
+          sink.close();
+        }
+      });
+    }
+  }, handleDone: (EventSink<T> sink) {
+    if (timer != null) {
+      shouldClose = true;
+    } else {
+      sink.close();
+    }
+  });
+}
---
+++ lib/src/scan.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import 'dart:async';
+
+typedef S Func2<T, S>(T item, S accumulation);
+
+StreamTransformer<T, S> scan<T, S>(Func2<T, S> map, S accumulation) =>
+    new _Scan(map, accumulation);
+
+class _Scan<T, S> implements StreamTransformer<T, S> {
+  final Func2<T, S> _map;
+  S _accumulation;
+
+  _Scan(this._map, this._accumulation);
+
+  @override
+  Stream<S> bind(Stream<T> source) {
+    return source.map((item) {
+      _accumulation = _map(item, _accumulation);
+      return _accumulation;
+    });
+  }
+}
