5387620837a1f2cdc3c2f718e4289bbf16f2aa50
Nate Bosch <nbosch@google.com>
2019-03-08 11:24:14 -0800

    Allow a `FutureOr` return in `scan`
    Closes #62
    
    Adds async support in `scan` instead of a new `asyncScan` since the
    latter would be able to serve both use cases so it would not be clear
    why both exist.
    
    Technically this is breaking but it's such an extreme edge case that it
    is very unlikely to come up in practice - and the breakage is detected
    statically. Due to the way `FuturOr<Future>` interacts with inference
    there are some cases where the generic types may need to be explicitly
    written. This is an unlikely scenario where the transformer emitted a
    stream of futures.

--- lib/src/scan.dart
+++ lib/src/scan.dart
@@ -6,9 +6,21 @@ import 'dart:async';
 
 /// Scan is like fold, but instead of producing a single value it yields
 /// each intermediate accumulation.
+///
+/// If [combine] returns a Future it will not be called again for subsequent
+/// events from the source until it completes, therefor the combine callback is
+/// always called for elements in order, and the result stream always maintains
+/// the same order as the original.
 StreamTransformer<S, T> scan<S, T>(
-        T initialValue, T combine(T previousValue, S element)) =>
-    StreamTransformer.fromBind((stream) {
+        T initialValue, FutureOr<T> combine(T previousValue, S element)) =>
+    StreamTransformer.fromBind((source) {
       var accumulated = initialValue;
-      return stream.map((value) => accumulated = combine(accumulated, value));
+      return source.asyncMap((value) {
+        var result = combine(accumulated, value);
+        if (result is Future<T>) {
+          return result.then((r) => accumulated = r);
+        } else {
+          return accumulated = result as T;
+        }
+      });
     });
